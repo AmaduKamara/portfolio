0d8013e36f4e0800599c61fe5a328c91c2d45f2b
Zemola <azeemolatunji@gmail.com>
2021-11-08 11:47:41 +0100

    Add javascript file and style the mobile menu

---
+++ app.js
@@ -0,0 +1,10 @@
+// get menu icon element
+const menuIcon = document.querySelector(".menu-icon")
+
+// get nav links
+const navLinks = document.querySelector(".nav-links")
+
+// event handler for the menu icon when clicked
+menuIcon.addEventListener('click', () => {
+    alert("JUST TESTING")
+})
