9234c727b3a0037f660b9e9b3ea9f6a278204bc6
Amkam <amaduskamara@gmail.com>
2021-11-08 17:35:26 +0000

    Remove previous styles for header

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // get menu icon element
-const menuIcon = document.querySelector(".menu-icon")
+const menuIcon = document.querySelector(".menu-icon");
 
 // get nav links
-const navLinks = document.querySelector(".nav-links")
+const navLinks = document.querySelector("ul");
 
 // event handler for the menu icon when clicked
-menuIcon.addEventListener('click', () => {
-    alert("JUST TESTING")
-})
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("visible");
+});
