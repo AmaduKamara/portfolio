845cb80f8399653fbc58c0f1e49307f0d3985dd8
Zemola <azeemolatunji@gmail.com>
2021-11-09 10:05:24 +0100

    Add styles for the close hamburger menu on mobile view and the javascript

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// get menu icon element
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav-menu");
 
+menu.addEventListener("click", () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
