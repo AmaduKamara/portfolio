464447e482c9ca5e653b450a760e57714458fcaf
Amkam <amaduskamara@gmail.com>
2021-11-09 09:13:13 +0000

    Edited nav links class names and add javascript code to close mobile menu when a link in clicked

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+// Get the bars of the hamburger menu
+const portfolio = document.querySelector(".portfolio-link");
+const aboutUs = document.querySelector(".about-link");
+const contactLink = document.querySelector(".contact-link");
+
+const close = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+portfolio.addEventListener("click", () => {
+  close();
+});
+
+aboutUs.addEventListener("click", () => {
+  close();
+});
+
+contactLink.addEventListener("click", () => {
+  close();
+});
