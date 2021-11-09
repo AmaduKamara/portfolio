777aa682d8bb7d5ae6a6a0c6a1c34ad3175e5486
Amkam <amaduskamara@gmail.com>
2021-11-09 08:37:33 +0000

    Changed navigation header structure

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-// get menu icon element
-const menuIcon = document.querySelector(".menu-icon");
 
-// get nav links
-const navLinks = document.querySelector("ul");
-
-// event handler for the menu icon when clicked
-menuIcon.addEventListener("click", () => {
-  navLinks.classList.toggle("visible");
-});
