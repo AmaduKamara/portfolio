9eb7b630b76a22a69b4ae6509adbc3bf3fd650b2
Amkam <amaduskamara@gmail.com>
2021-11-10 00:14:26 +0000

    Add Popup modal html. Add HTML structure for modal Styled the modal Parsed data from JS to html of the modal Worked on mobile responsive

---
+++ data.js
@@ -0,0 +1,62 @@
+const projects = [
+  {
+    name: 'Tonic',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './images/projects/Tonic-one.png',
+    technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+  {
+    name: 'Multi-Post Stores',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './images/projects/Multi-Post-One.png',
+    technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+  {
+    name: 'Facebook 360',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './images/projects/Facebook-360.png',
+    technologies: ['html', 'Ruby and rails', 'css', 'javascript'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+  {
+    name: 'Uber Navigator',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis deserunt nesciunt nulla tenetur ipsam dolorum placeat natus aperiam recusandae maiores.',
+    featuredImage: './images/projects/Uber-Navigator.png',
+    technologies: ['html', 'css', 'javascript', 'Ruby and rails'],
+    stacks: ['canopy', 'Backend Dev', '2015'],
+  },
+];
+
+// Tonic Modal Details
+const openTonicModal = document.querySelector('#open-tonic-modal');
+const tonicModalContainer = document.querySelector('.tonic-modal-container');
+const closeTonicModal = document.querySelector('#tonic-modal-close');
+
+openTonicModal.addEventListener('click', () => {
+  tonicModalContainer.classList.add('show');
+});
+
+closeTonicModal.addEventListener('click', () => {
+  tonicModalContainer.classList.remove('show');
+});
+
+document.querySelector('.tonic-title').textContent = projects[0].name;
+
+document.querySelector('.tonic-stack1').textContent =
+  projects[0].stacks[0].toUpperCase();
+document.querySelector('.tonic-stack2').textContent = projects[0].stacks[1];
+document.querySelector('.tonic-stack3').textContent = projects[0].stacks[2];
+document.querySelector('.tonic-description').textContent =
+  projects[0].description;
+document.querySelector('.tonic-image').src = projects[0].featuredImage;
+document.querySelector('.tonic-html').textContent = projects[0].technologies[0];
+document.querySelector('.tonic-css').textContent = projects[0].technologies[1];
+document.querySelector('.tonic-javascript').textContent =
+  projects[0].technologies[2];
+document.querySelector('.tonic-ruby').textContent = projects[0].technologies[3];
